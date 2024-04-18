1653ca9be1441467c5a0a1884848dd275f24af88
Protap Ghosh <protapg518@gmail.com>
2024-04-18 22:50:26 +0600

    added buy sponge token section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
-import NewUtilitiesAwait from '@/components/Sections/NewUtilitiesAwait/NewUtilitiesAwait';
-import WhyChooseSPONGE from '@/components/Sections/WhyChooseSponge/WhyChooseSponge';
 import React from 'react';
 import Banner from '@/components/Sections/Banner/Banner';
 import Roadmap from '@/components/Sections/Roadmap/Roadmap';
+import NewUtilitiesAwait from '@/components/Sections/NewUtilitiesAwait/NewUtilitiesAwait';
+import WhyChooseSPONGE from '@/components/Sections/WhyChooseSponge/WhyChooseSponge';
+import BuySpongeToken from '@/components/Sections/BuySpongeToken/BuySpongeToken';
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <BuySpongeToken />
       <Roadmap />
       <WhyChooseSPONGE />
       <NewUtilitiesAwait />
