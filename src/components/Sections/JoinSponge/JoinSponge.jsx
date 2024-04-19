9630230f45041b3a4dde9194571c9b8f524bc102
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-04-19 16:46:00 +0600

    aa

--- src/components/Sections/JoinSponge/JoinSponge.jsx
+++ src/components/Sections/JoinSponge/JoinSponge.jsx
@@ -5,23 +5,23 @@ import JoinSpongeborder from '@/assets/JoinSponge/div.elementor-background-overl
 
 const JoinSponge = () => {
   return (
-    <div className='xl:my-[140px]'>
+    <div className='xl:my-[140px] lg:my-[120px]'>
       <Container>
         {/* main section  */}
         <div className='relative'>
 
           {/* image section  */}
           <div className='flex justify-around items-center'>
-            <img className='2xl:w-auto z-40' src={JoinSpongeImg} alt="image" />
-            <p className='text-[#FFF] font-TTRoundsNeue xl:text-[40px] font-bold lg:leading-[68px] 2xl:w-[40%] z-40'>JOIN $SPONGE AND LETS BARK OUR  WAY TO CRYPTO SPONGE TOGETHER!</p>
+            <img className='xl:w-auto lg:w-[35%] z-40' src={JoinSpongeImg} alt="image" />
+            <p className='text-[#FFF] font-TTRoundsNeue xl:text-[40px] lg:text-[30px] font-bold lg:leading-[68px] lg:w-[45%] z-40'>JOIN $SPONGE AND LETS BARK OUR  WAY TO CRYPTO SPONGE TOGETHER!</p>
           </div>
 
           {/* moon section  */}
-          <div className='joinSpongeMoon absolute 2xl:top-[15px] 2xl:left-[630px]' />
+          <div className='joinSpongeMoon xl:h-[400px] lg:h-[300px] xl:w-[400px] lg:w-[300px] absolute 2xl:top-[2px] xl:top-[5px] lg:top-[-25px] 2xl:left-[510px] xl:left-[475px] lg:left-[375px]' />
 
           {/* border section  */}
-          <img className='2xl:w-auto absolute 2xl:right-[-30px] 2xl:top-[70px]' src={JoinSpongeborder} alt="" />
-          <img className='2xl:w-auto absolute 2xl:left-[-50px] 2xl:top-[-40px]' src={JoinSpongeborder} alt="" />
+          <img className='xl:w-auto absolute 2xl:right-[-50px] xl:right-[-50px] 2xl:top-[68px] xl:top-[68px]' src={JoinSpongeborder} alt="" />
+          <img className='xl:w-auto absolute 2xl:left-[-50px] xl:left-[-50px] 2xl:top-[-55px] xl:top-[-55px] z-50' src={JoinSpongeborder} alt="" />
 
         </div>
 
