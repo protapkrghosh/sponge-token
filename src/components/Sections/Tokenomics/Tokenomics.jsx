c57992ebe8b7d559886755c5a3bdedd1c2dc0d11
Protap Ghosh <protapg518@gmail.com>
2024-04-19 23:11:13 +0600

    added tokenomics sections

--- src/components/Sections/Tokenomics/Tokenomics.jsx
+++ src/components/Sections/Tokenomics/Tokenomics.jsx
@@ -8,66 +8,72 @@ const Tokenomics = () => {
 
   return (
     <div className='pt-44 pb-12' id='tokenomics'>
-      <Container>
-        <div>
-          <div className='-mb-[480px]'>
-            <h1 className='text-[#fff] text-[64px] font-TTRoundsNeue font-bold mb-9'>Tokeno<span className='text-[#FDF44E]'>mics</span></h1>
+      <div>
+        <Container>
+          <div className='relative'>
+            <div className='absolute bottom-12 xl:bottom-12'>
+              <h1 className='text-[#fff] text-[38px] lg:text-[55px] xl:text-[64px] font-TTRoundsNeue font-bold mb-9'>Tokeno<span className='text-[#FDF44E]'>mics</span></h1>
 
-            <div className='space-y-7'>
-              {/* Liquidity */}
-              <div className='flex items-center'>
-                <div className='bg-[#fdf44e] w-10 h-10 flex justify-center items-center rounded-full'>
-                  <IoMdCheckmark className='text-[#b1ab37] text-[22px]' />
+              <div className='space-y-5 xl:space-y-7'>
+                {/* Liquidity */}
+                <div className='flex items-center'>
+                  <div className='bg-[#fdf44e] w-6 xl:w-10 h-6 xl:h-10 flex justify-center items-center rounded-full'>
+                    <IoMdCheckmark className='text-[#b1ab37] text-[16px] xl:text-[22px]' />
+                  </div>
+
+                  <p className='text-white text-[22px] xl:text-[28px] font-bold font-poppins ml-4 xl:ml-6'>Liquidity <span className='text-[25px] xl:text-[32px] ml-3'>35%</span></p>
                 </div>
 
-                <p className='text-white text-[28px] font-bold font-poppins ml-6'>Liquidity <span className='text-[32px] ml-3'>35%</span></p>
-              </div>
+                {/* Team */}
+                <div className='flex items-center'>
+                  <div className='bg-[#c060a1] w-6 xl:w-10 h-6 xl:h-10 flex justify-center items-center rounded-full'>
+                    <IoMdCheckmark className='text-[#fff] text-[16px] xl:text-[22px]' />
+                  </div>
 
-              {/* Team */}
-              <div className='flex items-center'>
-                <div className='bg-[#c060a1] w-10 h-10 flex justify-center items-center rounded-full'>
-                  <IoMdCheckmark className='text-[#fff] text-[22px]' />
+                  <p className='text-white text-[22px] xl:text-[28px] font-bold font-poppins ml-4 xl:ml-6'>Team <span className='text-[25px] xl:text-[32px] ml-3'>5%</span></p>
                 </div>
 
-                <p className='text-white text-[25px] font-bold font-poppins ml-6'>Team <span className='text-[32px] ml-3'>5%</span></p>
-              </div>
+                {/* Future development */}
+                <div className='flex items-center'>
+                  <div className='bg-[#0147f7] w-6 xl:w-10 h-6 xl:h-10 flex justify-center items-center rounded-full'>
+                    <IoMdCheckmark className='text-[#fff] text-[16px] xl:text-[22px]' />
+                  </div>
 
-              {/* Future development */}
-              <div className='flex items-center'>
-                <div className='bg-[#0147f7] w-10 h-10 flex justify-center items-center rounded-full'>
-                  <IoMdCheckmark className='text-[#fff] text-[22px]' />
+                  <p className='text-white text-[22px] xl:text-[28px] font-bold font-poppins ml-4 xl:ml-6'>Future development <span className='text-[25px] xl:text-[32px] ml-3'>10%</span></p>
                 </div>
 
-                <p className='text-white text-[25px] font-bold font-poppins ml-6'>Future development <span className='text-[32px] ml-3'>10%</span></p>
-              </div>
+                {/* Staking  rewards */}
+                <div className='flex items-center'>
+                  <div className='bg-[#2bbf7f] w-6 xl:w-10 h-6 xl:h-10 flex justify-center items-center rounded-full'>
+                    <IoMdCheckmark className='text-[#fff] text-[16px] xl:text-[22px]' />
+                  </div>
 
-              {/* Staking  rewards */}
-              <div className='flex items-center'>
-                <div className='bg-[#2bbf7f] w-10 h-10 flex justify-center items-center rounded-full'>
-                  <IoMdCheckmark className='text-[#fff] text-[22px]' />
+                  <p className='text-white text-[22px] xl:text-[28px] font-bold font-poppins ml-4 xl:ml-6'>Staking  rewards <span className='text-[25px] xl:text-[32px] ml-3'>20%</span></p>
                 </div>
 
-                <p className='text-white text-[25px] font-bold font-poppins ml-6'>Staking  rewards <span className='text-[32px] ml-3'>20%</span></p>
-              </div>
+                {/* Presale */}
+                <div className='flex items-center'>
+                  <div className='bg-[#6d67e4] w-6 xl:w-10 h-6 xl:h-10 flex justify-center items-center rounded-full'>
+                    <IoMdCheckmark className='text-[#fff] text-[16px] xl:text-[22px]' />
+                  </div>
 
-              {/* Presale */}
-              <div className='flex items-center'>
-                <div className='bg-[#6d67e4] w-10 h-10 flex justify-center items-center rounded-full'>
-                  <IoMdCheckmark className='text-[#fff] text-[22px]' />
+                  <p className='text-white text-[22px] xl:text-[28px] font-bold font-poppins ml-4 xl:ml-6'>Presale <span className='text-[25px] xl:text-[32px] ml-3'>30%</span></p>
                 </div>
-
-                <p className='text-white text-[25px] font-bold font-poppins ml-6'>Presale <span className='text-[32px] ml-3'>30%</span></p>
               </div>
             </div>
-          </div>
 
-
-          {/* Pie chart area */}
-          <div>
-            <img src={chatImg} alt="Image" className='2xl:min-w-[1370px]'/>
+            {/* Pie chart area */}
+            <div>
+              <img src={chatImg} alt="Image" className='lg:min-w-[990px] xl:min-w-[1250px] 2xl:min-w-[1370px]' />
+            </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+
+        {/* Shadow */}
+        {/* <div>
+          <div className='tokenShadow w-[370px] h-[370px] absolute right-0' />
+        </div> */}
+      </div>
     </div>
   );
 };
